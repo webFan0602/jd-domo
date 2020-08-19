9567ed2c56781d6a53d7f53eda54cdc9626f95c6
liuguanyu <liu2795049474@163.com>
2020-08-19 11:56:33 +0800

    修改了luo的内容

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-<<<<<<< HEAD
-=======
     <h6>我是罗浩滨</h6>
->>>>>>> ff9d280cdb2daf24a5738a7f6a60d1903268daa1
   </div>
 </template>
 
